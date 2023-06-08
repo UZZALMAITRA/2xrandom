b8f6c4171eaf03acaf91326d8c10e4dd5ad42def
UZZALMAITRA <129254709+UZZALMAITRA@users.noreply.github.com>
2023-06-08 17:27:19 +0530

    Add files via upload

--- fuck.py
+++ fuck.py
@@ -109,6 +109,7 @@ def fuck():
     os.system('clear')
     os.system('xdg-open https://www.facebook.com/gsriyad11/')
     print(logo)
+    print("\033[38;5;46--------------------------------------------")
     print("\x1b[1;91m⧼ WELCOME MY FRIENDS RANDOM CLONE ENJOY ⧽")
     print("\033[38;5;46--------------------------------------------")
     print('\033[1;32m[+] SIM CODE BD=> 016 • 017 • 018 • 019 • 013')
