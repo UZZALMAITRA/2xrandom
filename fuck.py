431125d400e9006db2c51a544e677e66546a721d
UZZALMAITRA <129254709+UZZALMAITRA@users.noreply.github.com>
2023-06-08 17:44:13 +0530

    Add files via upload

--- fuck.py
+++ fuck.py
@@ -166,8 +166,6 @@ def rcrack(uid,pwx,tl):
             header_freefb = {'authority': 'm.facebook.com',
    		 'method': 'GET',
 		    'scheme': 'https', 
-		    headers = {
-		    'authority': 'm.facebook.com',
 		    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 		    'accept-language': 'en-US,en;q=0.9',
 		    'cache-control': 'max-age=0',
